be61fd240ba6784fdea549a9117d1ff021c7b726
julian rudolph <julian.rudolphsc@gmail.com>
2016-07-04 09:57:53 -0500

    opps

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,8 +2,10 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <h3>About</h3>
-    <p>This is the welcome to the about page paragraph.  Who the fuck knows, or cares really, what is here?  I sure as fuck dont...just saying.  All this to make sure i can push to github and heroku properly.  WEll, fuck, lets do it.</p>
+    <div>
+      <h3>About</h3>
+      <p>This is the welcome to the about page paragraph.  Who the fuck knows, or cares really, what is here?  I sure as fuck dont...just saying.  All this to make sure i can push to github and heroku properly.  WEll, fuck, lets do it.</p>
+    </div>
   )
 };
 
